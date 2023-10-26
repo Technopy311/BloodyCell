8258a5e7b5ab540955e50d8a5935523f760289bf
technopy311 <xmenmagnet@gmail.com>
2023-10-26 00:03:43 -0300

    undo seek remove

--- core/static/core/arcade/Core.js
+++ core/static/core/arcade/Core.js
@@ -247,6 +247,7 @@ class Core extends Phaser.Scene {
             volume: 1,
             rate: 1,
             detune: 0,
+            seek: 0,
             loop: false,
             delay: 0
         }

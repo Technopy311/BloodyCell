7856f7f3b1580ce5f1daef14c4a8a1a75ebf4fe7
technopy311 <xmenmagnet@gmail.com>
2023-10-26 10:28:12 -0300

    setup admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import RankingEntry
+
+class RankingAdmin(admin.ModelAdmin):
+    fields = ["student", "grade", "score", "date_time"]
+
+admin.site.register(RankingEntry, RankingAdmin)
